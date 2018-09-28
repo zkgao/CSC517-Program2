0f9c4efe14072ab2861a2576490eead8d2731c2a
ychosen <ych_327@hotmail.com>
2018-09-27 23:47:40 -0400

    add view user list and change homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :houses
   devise_for :users
+  resources :users
   resources :real_estate_companies
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
