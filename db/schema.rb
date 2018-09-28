d65c1442bf685d2c7937d48313227a846f69e054
ychosen <ych_327@hotmail.com>
2018-09-28 00:55:50 -0400

    user add name, edit profile complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_223612) do
+ActiveRecord::Schema.define(version: 2018_09_28_043222) do
 
   create_table "admins", primary_key: "email", force: :cascade do |t|
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_223612) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
