c395287385fbcf69271ac00bd34ca35417db24dd
ychosen <ych_327@hotmail.com>
2018-09-28 02:39:31 -0400

    complete user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,67 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+  end
+
+  # GET /real_estate_companies/new
+  def new
+    @user= User.new
+  end
+
+  # GET /real_estate_companies/1/edit
+  def edit
+  end
+
+  # POST /real_estate_companies
+  # POST /real_estate_companies.json
+  def create
+    @user= User.new(user_params)
+
+    respond_to do |format|
+      if @real_estate_company.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user}
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /real_estate_companies/1
+  # PATCH/PUT /real_estate_companies/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user}
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /real_estate_companies/1
+  # DELETE /real_estate_companies/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to iser_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:Id, :Name, :Email)
+  end
 end
--- app/models/real_estate_company.rb
+++ app/models/real_estate_company.rb
@@ -1,2 +1,11 @@
 class RealEstateCompany < ApplicationRecord
+
+  enum sizes: {
+      '1~50' => 0,
+      '51~200' => 1,
+      '201~500' => 2,
+      '501~1000' => 3,
+      '1000+' => 4
+  }
+  validates :Size, inclusion: sizes.keys
 end
