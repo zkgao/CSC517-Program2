f9952ea467745c67c154d36dd1fd3fe5d5780cef
ychosen <ych_327@hotmail.com>
2018-10-01 01:09:55 -0400

    structure done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,6 @@ class UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:Id, :Name, :Email)
+    params.require(:user).permit(:Id, :Name, :email)
   end
 end
