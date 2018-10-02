23bcb9a24b2d6c4ca6a2774945d055894b5570e3
zgao9 <zgao9@ncsu.edu>
2018-10-01 21:07:09 -0400

    structure done

--- db/migrate/20180929150206_create_houses.rb
+++ db/migrate/20180929150206_create_houses.rb
@@ -1,7 +1,15 @@
 class CreateHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :houses do |t|
-
+        t.string :location
+        t.string :square_footage
+        t.integer :year_built
+        t.string :style, limit: 19
+        t.integer :list_price
+        t.integer :floors
+        t.string :basement, limit: 3
+        t.string :current_house_owner
+        t.string :contact_information_for_listing_realtor
       t.timestamps
     end
   end
--- db/migrate/20181001221110_drop_houses_table.rb
+++
@@ -1,8 +0,0 @@
-class DropHousesTable < ActiveRecord::Migration[5.2]
-  def up
-      drop_table :houses
-  end
-  def down
-      fail ActiveRecord::IrreversibleMigration
-  end
-end
