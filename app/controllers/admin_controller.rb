4ab1f5657d59f313d96dbc5c548b592db5ec522e
ychosen <ych_327@hotmail.com>
2018-10-07 02:09:25 -0400

    add name to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,26 @@
 class AdminController < ApplicationController
+
+
+  def new
+  end
+
+  def create
+    super
+  end
+
+  def update
+    respond_to do |format|
+      if @admin.update(user_params)
+        format.html { redirect_to @admin, notice: 'Admin was successfully updated.' }
+        format.json { render :show, status: :ok, location: @admin}
+      else
+        format.html { render :edit }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def user_params
+    params.require(:admin).permit(:Id, :Name, :email)
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
   def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       adminmanage_url
