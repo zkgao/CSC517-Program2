c7121431978e84f78bac8896ec48a928fb20fae2
Sauve-moi <yzzczy@gmail.com>
2018-10-10 12:31:35 -0400

    Access control for realtor; fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def realtor?
+    if ( companyid == nil )
+      false
+    else true
+    end
+  end
+
 end
