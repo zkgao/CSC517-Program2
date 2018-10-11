f7ec420429aeabe95705d39726b5a6421c0274c7
zgao9 <zgao9@ncsu.edu>
2018-10-10 20:53:38 -0400

    inquiry fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_035042) do
+ActiveRecord::Schema.define(version: 2018_10_10_063020) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
