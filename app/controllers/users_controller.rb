89577669c1118bff136078aab480be9f0cce871c
zgao9 <zgao9@ncsu.edu>
2018-10-10 20:48:06 -0400

    change index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,9 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+def showinquiry
+  @inquiries=Inquiry.where(userid: params[:id])
+end
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
