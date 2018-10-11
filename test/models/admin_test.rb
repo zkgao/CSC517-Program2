404eecd7b281d1e1a134779c4989238a066fb692
Sauve-moi <yzzczy@gmail.com>
2018-10-11 16:22:42 -0400

    Update admin_test.rb

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -4,4 +4,17 @@ class AdminTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # end
+  def setup
+    @admin = Admin.new(email: "24@12414.com" )
+  end
+
+
+  test "email addresses should be unique" do
+    duplicate_user = @admin.dup
+    @admin.save
+    assert_not duplicate_user.valid?
+  end
+
 end
+
